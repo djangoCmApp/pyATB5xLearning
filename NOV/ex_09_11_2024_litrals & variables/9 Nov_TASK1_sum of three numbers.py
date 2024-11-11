4a296d61205adb6dc9af75c51d318c5985010aeb
kavya.srinivaskumar <skavya93@gmail.com>
2024-11-11 21:18:15 +0530

    push

---
+++ NOV/ex_09_11_2024_litrals & variables/9 Nov_TASK1_sum of three numbers.py
@@ -0,0 +1,14 @@
+num1 =int(input('enter first num'))
+num2 =int(input('enter second num'))
+num3 =int(input('enter third num'))
+
+sum = num1+num2+num3
+sub= num1-num2-num3
+prod =num1*num2*num3
+div =num1/num2/num3
+
+
+print ('sum of entered numbers ',sum)
+print ('difference of entered numbers ',sub)
+print ('product of entered numbers ',prod)
+print ('division of entered numbers ',div)
---
+++ NOV/ex_09_11_2024_litrals & variables/9Nov_TASK 2 _reminder and quotient.py
@@ -0,0 +1,11 @@
+from math import remainder
+
+num1 = 89
+num2 = 7
+
+remainder = num1 % num2
+quotient = num1 // num2
+
+print('The quotient is :',quotient)
+print('The remainder is :',remainder)
+
