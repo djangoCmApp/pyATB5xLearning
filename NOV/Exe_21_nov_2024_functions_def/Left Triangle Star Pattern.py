b00ff6df6930b432379a022f411fabad81c19b97
kavya.srinivaskumar <skavya93@gmail.com>
2024-11-27 22:05:39 +0530

    task 21st nov

---
+++ NOV/Exe_21_nov_2024_functions_def/Left Triangle Star Pattern.py
@@ -0,0 +1,12 @@
+a =int(input('enter the number'))
+b = a
+
+for i in range (a):
+    for j in range(b):
+        print('*',end =' ')
+    print()
+    b =b-1
+
+
+
+
---
+++ NOV/Exe_21_nov_2024_functions_def/Right Triangle Star Pattern.py
@@ -0,0 +1,5 @@
+
+a =int(input('enter the number'))
+for i in range (a):
+    for j in range(i,i+1):
+        print(i * '*')
